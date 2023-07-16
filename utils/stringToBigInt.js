87ef5223d57279981433c473a2ab989f761bb9c2
Yunus Gürlek <yunusgurlek@Yunus-MacBook-Pro.local>
2023-07-16 18:17:40 +0200

    Fix stringToBigInt logic to avoid hash collisions

--- utils/stringToBigInt.js
+++ utils/stringToBigInt.js
@@ -1,12 +1,29 @@
+const FORMATTED_CHAR_LENGTH = 4;
+
+// Add extra 0 to the left of the char code if it's length is less than FORMATTED_CHAR_LENGTH
+function formatChar(_char) {
+  let char = _char;
+
+  while (char.length < FORMATTED_CHAR_LENGTH)
+    char = '0' + char;
+
+  return char;
+};
+
+// Take the mod of the char code with 10^FORMATTED_CHAR_LENGTH. This is an extra check in case there are some ASCII codes with more than FORMATTED_CHAR_LENGTH digits
+function formatCharCode(charCode) {
+  return (charCode % Math.pow(10, FORMATTED_CHAR_LENGTH + 1)).toString();
+};
+
 module.exports = str => {
   if (!str || typeof str != 'string' || !str.trim().length)
     return null;
 
-  let result = 0n;
+  let resultString = '';
 
   str.trim().split('').forEach(char => {
-    result += BigInt(char.charCodeAt(0));
+    resultString += formatChar(formatCharCode(char.charCodeAt(0)));
   });
 
-  return result;
+  return BigInt(resultString);
 }
